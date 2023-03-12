35939aa0c73113622c5b72bcdcb7ad2754f7fbe2
alanhe <qianghe421@gmail.com>
2023-03-12 19:16:59 +0800

    update bing-edge-ua.sgmodule

--- bing-edge-ua.js
+++ bing-edge-ua.js
@@ -1,11 +1,10 @@
 (async () => {
   const headers = $request.headers;
-
   delete headers['user-agent'];
   delete headers['sec-ch-ua-full-version'];
   delete headers['sec-ch-ua-full-version-list'];
-
   if ($environment.system === 'macOS') {
+    // macOS
     headers['User-Agent'] =
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.50';
     headers['sec-ch-ua'] =
@@ -13,8 +12,13 @@
     headers['sec-ch-ua-mobile'] = '?0';
     headers['sec-ch-ua-platform'] = 'macOS';
   } else {
+    // iOS
     headers['User-Agent'] =
-    'Mozilla/5.0 (iPhone; CPU iPhone OS 16_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Mobile/15E148 Safari/605.1.15 BingSapphire/1.0.410205001';
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 16_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Mobile/15E148 Safari/605.1.15 BingSapphire/1.0.410218002';
+    headers['sec-ch-ua-mobile'] = '?1';
+    headers['sec-ch-ua-platform'] = 'iOS';
   }
+  console.log($environment.system);
+  console.log(headers['User-Agent']);
   $done({ headers });
 })();
