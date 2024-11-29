987fc2d53f3f4277689d1d99ffc43667fe5c1fe6
Alan He <qianghe421@gmail.com>
2024-11-29 21:27:52 +0800

    fix(fantastical.sgmodule): 更新Fantastical脚本路径为GitHub raw内容链接

--- fantastical.js
+++ fantastical.js
@@ -3,40 +3,25 @@
 hostname=api.flexibits.com
 */
 
-let url=$request.url;
-let obj=JSON.parse($response.body);
-
-if(url.indexOf('device')!=-1){
-
-  obj.status="success";
-  obj.subscription={
-	  	"autorenew": true,
-		"expiration": "2099-12-31T16:49:37Z",
-		"expires": "2099-12-31T16:49:37Z"
-  };
-  obj.scope=["notify", "weather", "keyvalue-watch", "keyvalue-verification", "schedjoules", "scheduling", "account", "keyvalue", "fantastical"];
-  
-}
-
-if(url.indexOf('details')!=-1){
-
-  obj.subscription={
+let url = $request.url;
+const path1="v1/auth/device";
+const path2="v1/account/details";
+let obj = JSON.parse($response.body);
+if (url.indexOf(path1) > -1) {
+	obj["subscription"] = {
 		"autorenew": true,
-		"expiration": "2099-12-31T16:49:37.000000Z",
-		"uuid": "f1da7c78-e964-4367-915c-886edc794959",
+		"expiration": "2099-02-15T09:09:14Z",
+		"expires": "2099-02-15T09:09:14Z"
+	}
+}
+if (url.indexOf(path2) > -1) {
+	obj["subscription"]={
+		"autorenew": true,
+		"expiration": "2099-02-15T09:09:14.000000Z",
+		"uuid": "3c5927a6-4e3e-4773-b7af-62506e036dc7",
 		"subscription_type": "AppStore",
 		"is_expired": false,
 		"trial": false
-  };
+	}
 }
-
-if(url.indexOf('appstore-receipt')!=-1){
-	
-	obj.autorenew=true;
-	obj.expiration="2099-12-31T16:49:37.000000Z";
-	obj.subscription_type="AppStore";
-	obj.is_expired=false;
-	obj.trial=false;
-}
-
-$done({body:JSON.stringify(obj)});
+$done({body: JSON.stringify(obj)});
